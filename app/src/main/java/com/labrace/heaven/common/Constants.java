c52e2ff297daa55bcf9e4f6a2a8f9dfac9e0738e
chienbh <chienbh@toprate.io>
2022-10-10 11:45:19 +0700

    first commit

---
+++ app/src/main/java/com/labrace/heaven/common/Constants.java
@@ -0,0 +1,18 @@
+package com.labrace.heaven.common;
+
+/**
+ * Created by Dung on 4/18/2016.
+ */
+public class Constants {
+    public static final String PATIENT = "patient";
+    public static final String USER_ID = "user_id";
+    public static final String USER_NAME = "user_name";
+    public static final String IMAGE = "image";
+    public static final String DATE_FORMAT = "MM/dd/yyyy";
+    public static final int IMAGE_MAX_SIZE = 1000;
+
+    public static final String SCREEN_PATIENT_LIST = "com.labrace.heaven.fragment.PatientListFragment";
+    public static final String SCREEN_NEW_PATIENT = "com.labrace.heaven.fragment.AddNewPatientFragment";
+    public static final String SCREEN_PHOTO_DETAIL = "com.labrace.heaven.fragment.PatientPhotoDetailFragment";
+    public static final String SCREEN_PHOTO_LIST = "com.labrace.heaven.fragment.PatientPhotoListFragment";
+}
---
+++ app/src/main/java/com/labrace/heaven/common/LoginStatus.java
@@ -0,0 +1,11 @@
+package com.labrace.heaven.common;
+
+/**
+ * Created by Dung on 4/22/2016.
+ */
+public enum LoginStatus {
+    EXCEPTION,
+    INVALID_USERNAME_OR_PASSWORD,
+    ACCOUNT_DISABLED,
+    SUCCESS
+}
